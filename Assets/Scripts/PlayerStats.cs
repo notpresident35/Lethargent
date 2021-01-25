4c8d71fe81921bd75adbf5cbe01dbca502587a70
Wazyre <32106559+Wazyre@users.noreply.github.com>
2021-01-25 01:07:59 -0800

    Add ability to save key collection

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -13,6 +13,9 @@ public class PlayerStats
 
     int m_sceneID = 0; //Default scene
 
+    // Collectibles dictionary with {collectible_id (String) : collected (Bool)}
+    private Dictionary<string, bool> m_collectibles = new Dictionary<string, bool>();
+
     public float playerPosX
     {
       get{return m_playerPosX;}
@@ -32,5 +35,29 @@ public class PlayerStats
     {
       get{return m_sceneID;}
       set{m_sceneID = value;}
+    }
+    public Dictionary<string, bool> collectibles
+    {
+       get{return m_collectibles;}
+    }
+
+    public bool collectibleRegistered(string id)
+    {
+        return m_collectibles.ContainsKey(id);
+    }
+
+    public void addCollectible(string id, bool value=false)
+    {
+        m_collectibles.Add(id, value);
+    }
+
+    public void updateCollectibles(string id, bool new_value)
+    {
+        m_collectibles[id] = new_value;
+    }
+
+    public bool getCollectibleItem(string id)
+    {
+        return m_collectibles[id];
     }
 }
