b5b845f511f7becf30cbe5a5e5751af03aa45721
notpresident35 <notpresident35@gmail.com>
2021-01-26 09:12:08 -0800

    UI Billboarding
    UI scales and rotates to face the camera

---
+++ Assets/Scripts/UIBillboard.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIBillboard : MonoBehaviour {
+
+    [SerializeField] float scaleFactor;
+    [SerializeField] float maxScale;
+    [SerializeField] float minScale;
+
+    Transform camTransform;
+    Quaternion originalRotation;
+    Vector3 originalScale;
+
+    void Start () {
+        originalRotation = transform.rotation;
+        camTransform = Camera.main.transform;
+        originalScale = transform.localScale;
+    }
+
+    void Update () {
+        transform.rotation = camTransform.rotation * originalRotation;
+        transform.localScale = Mathf.Clamp ((camTransform.position - transform.position).magnitude * scaleFactor, minScale, maxScale) * originalScale;
+    }
+}
