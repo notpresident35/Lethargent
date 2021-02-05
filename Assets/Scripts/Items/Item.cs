8e9db012aac3fa90a4839e169717b2db3349c378
Wazyre <32106559+Wazyre@users.noreply.github.com>
2021-02-05 08:30:11 -0800

    Finish item system and base item class
    If NPCs or other objects wish to interact based on carried items, they can access them through Levelmanager.current.playerData.getItem

---
+++ Assets/Scripts/Items/Item.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Item : MonoBehaviour
+{
+    private string name;
+    private int id;
+    private bool normal;
+
+    public Item(string _name, int _id, bool _norm)
+    {
+        name = _name;
+        id = _id;
+        normal = _norm;
+    }
+
+    public virtual void PickUp()
+    {
+        LevelManager.current.playerData.UpdateItem(this, true);
+    }
+
+    public virtual void Drop()
+    {
+        if (normal)
+        {
+            LevelManager.current.playerData.UpdateItem(this, false);
+        }
+    }
+
+    public virtual void Use()
+    {
+        if (normal)
+        {
+            Action();
+        }
+    }
+
+    public virtual void Action() {}
+}
