716a71a6e09e7151b0bc926a659a659a46196a75
notpresident35 <notpresident35@gmail.com>
2021-04-08 00:22:23 -0700

    Cutscene work

---
+++ Assets/Scripts/Interactables/Kanna.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kanna : GenericInteractable {
+
+    public override void Interact () {
+        CutsceneManager.Singleton.StartCutscene (2);
+    }
+}
